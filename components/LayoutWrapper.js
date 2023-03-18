eaf1dcf11038aef684332ed2a1c6b60d46d72d28
Isaac Tait <isaac@mountaintopcoding.com>
2023-03-18 14:35:24 -0400

    chore: merge section container back into layout 🥐

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -10,47 +10,49 @@ import ThemeSwitch from './ThemeSwitch'
 
 const LayoutWrapper = ({ children }) => {
   return (
-    <div className="flex h-screen flex-col justify-between">
-      <header className="flex items-center justify-between py-10">
-        {/* Desktop Logo */}
-        <div className="hidden md:block">
-          <Link href="/" aria-label={siteMetadata.headerTitle}>
-            <div className="flex items-center justify-between">
-              <div className="mr-3">
-                <Logo />
+    <SectionContainer>
+      <div className="flex h-screen flex-col justify-between">
+        <header className="flex items-center justify-between py-10">
+          {/* Desktop Logo */}
+          <div className="hidden md:block">
+            <Link href="/" aria-label={siteMetadata.headerTitle}>
+              <div className="flex items-center justify-between">
+                <div className="mr-3">
+                  <Logo />
+                </div>
               </div>
-            </div>
-          </Link>
-        </div>
-        {/* Mobile Logo */}
-        <div className="block md:hidden">
-          <Link href="/" aria-label={siteMetadata.headerTitle}>
-            <div className="mr-3">
-              <MobileLogo />
-            </div>
-          </Link>
-        </div>
-        <div className="flex items-center text-base leading-5">
-          <div className="hidden sm:block">
-            {headerNavLinks.map((link) => (
-              <Link
-                key={link.title}
-                href={link.href}
-                className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
-              >
-                {link.title}
-              </Link>
-            ))}
+            </Link>
           </div>
-          <ThemeSwitch />
-          <MobileNav />
+          {/* Mobile Logo */}
+          <div className="block md:hidden">
+            <Link href="/" aria-label={siteMetadata.headerTitle}>
+              <div className="mr-3">
+                <MobileLogo />
+              </div>
+            </Link>
+          </div>
+          <div className="flex items-center text-base leading-5">
+            <div className="hidden sm:block">
+              {headerNavLinks.map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </div>
+            <ThemeSwitch />
+            <MobileNav />
+          </div>
+        </header>
+        <main className="grow">{children}</main>
+        <div className="bottom-0">
+          <Footer />
         </div>
-      </header>
-      <main className="grow bg-slate-500">{children}</main>
-      <div className="bottom-0">
-        <Footer />
       </div>
-    </div>
+    </SectionContainer>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,13 @@ export default function Home() {
   return (
     <div>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
+      {/* 
+      I want this to flex grow into the space created on line 50 of layoutWrapper.js 
+      I tried h-full, h-fit, grow, and even h-5/6 to get the div below to grow into the space it has... 
+      Nothing seems to be working.
+      */}
       <div className="rounded-lg bg-blue-200 shadow-lg">
-        <p className="text-center">placeholder</p>
+        <p className="h-full text-center">placeholder</p>
       </div>
     </div>
   )
