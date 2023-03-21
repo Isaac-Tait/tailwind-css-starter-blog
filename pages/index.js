787408cc074c425a06c472195d0a0965f554bd8c
Isaac Tait <isaac@mountaintopcoding.com>
2023-03-21 09:12:03 -0400

    feat: victory is mine grow flex grow 🥑

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,23 @@ import siteMetadata from '@/data/siteMetadata'
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-full rounded-lg bg-blue-200 shadow-lg dark:bg-slate-600">
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       {/* 
       I want this to flex grow into the space created on line 50 of layoutWrapper.js 
       I tried h-full, h-fit, grow, and even h-5/6 to get the div below to grow into the space it has... 
       Nothing seems to be working.
       */}
-      <div className="rounded-lg bg-blue-200 shadow-lg">
-        <p className="h-full text-center">placeholder</p>
+      <div className="flex h-full flex-wrap content-center">
+        <p className="mx-auto w-1/2 justify-center text-xl font-semibold md:text-5xl">
+          Hello{' '}
+          <span role="img" aria-label="waving hand">
+            &#128075;
+          </span>
+          ,&nbsp;and welcome to my portfolio. My name is Isaac Tait and I am an activist web
+          developer who loves the outdoors.
+          <div className="-mt-48 -ml-8 h-64 w-64 rounded-full bg-blue-300 dark:bg-slate-400"></div>
+        </p>
       </div>
     </div>
   )
